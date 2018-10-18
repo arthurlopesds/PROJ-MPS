53980296f9dd9424fe2158978a89d740664a9900
yuregl <yuregaldino@hotmail.com>
2018-10-18 16:15:21 -0300

    Adição de InfraEstrututa e HorarioSala

---
+++ src/business/model/HorariosSala.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.model;
+
+/**
+ *
+ * @author Yure
+ */
+public class HorariosSala {
+    private Data date;
+    private String[][] aula = new String[8][6];
+    private Hora hora;
+}
---
+++ src/business/model/InfraEstrutura.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.model;
+
+/**
+ *
+ * @author Yure
+ */
+public class InfraEstrutura {
+    private String nome;
+    private String status;
+
+    public InfraEstrutura(String nome, String status) {
+        this.nome = nome;
+        this.status = status;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "O "  + nome + ", está " + status ;
+    }
+}
