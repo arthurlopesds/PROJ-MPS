8e300b2ec53da457b78ac91f716eb98480f20205
arthurlopesds <arthurlds2@gmail.com>
2018-10-18 16:46:32 -0300

    Adição do Método PrintHorario
    Método para Impressão do Horário da Sala

--- src/business/model/HorariosSala.java
+++ src/business/model/HorariosSala.java
@@ -13,4 +13,44 @@ public class HorariosSala {
     private Data date;
     private String[][] aula = new String[8][6];
     private Hora hora;
+
+    public Data getDate() {
+        return date;
+    }
+
+    public void setDate(Data date) {
+        this.date = date;
+    }
+
+    public String[][] getAula() {
+        return aula;
+    }
+
+    public void setAula(String[][] aula) {
+        this.aula = aula;
+    }
+
+    public Hora getHora() {
+        return hora;
+    }
+
+    public void setHora(Hora hora) {
+        this.hora = hora;
+    }
+
+    public void PrintHorario(){
+
+        for (int i=0; i<this.aula.length;i++){
+            for(int j=0; j<this.aula.length;j++){
+		System.out.print(this.aula[j][i]+" - ");
+}
+        System.out.println();
+}
+
+        
+}
+
+    
+    
+    
 }
