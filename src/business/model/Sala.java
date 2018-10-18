0c7d679bf3bb7a905d932842857a351776567c33
yuregl <yuregaldino@hotmail.com>
2018-10-18 15:13:33 -0300

    Edição de classe Sala
    Adicionados atributos  e metódos get e set.

--- src/business/model/Sala.java
+++ src/business/model/Sala.java
@@ -10,5 +10,26 @@ package business.model;
  * @author debor
  */
 public class Sala {
+    private String numeroSala;
+    //private InfraEstrutura infra; 
+    //private HorariosSala horario;
+    private boolean statusSala;
+
+    public String getNumeroSala() {
+        return numeroSala;
+    }
+
+    public void setNumeroSala(String numeroSala) {
+        this.numeroSala = numeroSala;
+    }
+
+    public boolean isStatusSala() {
+        return statusSala;
+    }
+
+    public void setStatusSala(boolean statusSala) {
+        this.statusSala = statusSala;
+    }
+    
     
 }
