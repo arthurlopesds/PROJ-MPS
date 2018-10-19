0865b37c29ebf1237d1b374d31603eacbe1fc218
arthurlopesds <arthurlds2@gmail.com>
2018-10-18 22:49:49 -0300

    Adição de Padrão de Projeto
    Foi Adicionado o Padrão de Projeto Template Method no Relatório

--- src/infra/Relatorio.java
+++ src/infra/Relatorio.java
@@ -1,5 +1,20 @@
 package infra;
 
 public abstract class Relatorio {
+    
+    final void Relat(){
+       qtdeAcesso();
+       GerarArquivo();
+  
+    }
+    
     public abstract void GerarArquivo();
+    
+    final void qtdeAcesso(){
+        
+        System.out.println("15 acessos pelo aplicativo nos últimos 3 dias\n");
+        System.out.println("45 acessos pelo SIGAA nos ultimos 2 dias");
+    
+    }
+
 }
