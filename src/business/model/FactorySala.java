f3fdae844006fe17a3b1f4e3a44bad1076978279
Déborah Pereira <debora_a_pereira@hotmail.com>
2018-11-06 23:58:21 -0200

    Update src/business/model/FactorySala.java

--- src/business/model/FactorySala.java
+++ src/business/model/FactorySala.java
@@ -2,17 +2,13 @@ package business.model;
 
 public class FactorySala {
     public Sala getSala(String nomeSala, InfraEstrutura infra, HorariosSala horario, boolean statusSala, String tipoSala){
- 	if(tipoSala.equalsIgnoreCase("Auditorio")){
+ 	if(tipoSala.equalsIgnoreCase("Auditorio"))
             return new Auditorio(nomeSala,infra,horario,statusSala);
- 	}
- 	else if(tipoSala.equalsIgnoreCase("Laboratorio")){
+ 	else if(tipoSala.equalsIgnoreCase("Laboratorio"))
             return new Lab(nomeSala,infra,horario,statusSala);
- 	}
-	else if(tipoSala.equalsIgnoreCase("Sala de Aula")){
+	else if(tipoSala.equalsIgnoreCase("Sala de Aula"))
             return new SaladeAula(nomeSala,infra,horario,statusSala);
- 	}
-        return null;
-         		
+        return null;		
     }
-    }
+}
 
